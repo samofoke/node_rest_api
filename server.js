af63cafde48e901b3fcbd538233f2d475c39cbca
samofoke <samofoke@student.wethinkcode.co.za>
2020-11-03 14:39:17 +0200

    200 and 404

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const http = require('http')
+const prod = require('./data/products.json')
 
 //creating a method that create a request and response
 const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Yype', 'text/html')
-    res.write('<h1>I am alive server</h1>')
-    console.log("I am connected...!!!!")
-    res.end()
+    if (req.url === '/products') {
+        res.writeHead(200, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify(prod)) //we get it as a string
+    }else {
+        res.writeHead(404, {'Content-Type': 'application/json'})
+        res.end(JSON.stringify({mesage: 'Route Not Found.'}))
+    }  
 })
 
 const PORT = process.env.PORT || 5000
